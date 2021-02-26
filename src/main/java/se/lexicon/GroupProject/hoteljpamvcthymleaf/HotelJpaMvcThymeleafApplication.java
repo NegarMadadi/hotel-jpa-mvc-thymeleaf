399e6334d78f541266e33c6577192226baa1ebeb
Negar <negar.myeah@gmail.com>
2021-02-26 10:43:34 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/GroupProject/hoteljpamvcthymleaf/HotelJpaMvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.GroupProject.hoteljpamvcthymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelJpaMvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelJpaMvcThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/lexicon/GroupProject/hoteljpamvcthymleaf/HotelJpaMvcThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.GroupProject.hoteljpamvcthymleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelJpaMvcThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
