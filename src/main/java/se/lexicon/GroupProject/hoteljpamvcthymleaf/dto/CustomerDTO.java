9d3d46472705d7a798457daf317c901a00a0bbf5
Negar <negar.myeah@gmail.com>
2021-03-01 12:41:38 +0100

    Created CustomerDTO.java and tested.

--- src/main/java/se/lexicon/GroupProject/hoteljpamvcthymleaf/dto/CustomerDTO.java
+++ src/main/java/se/lexicon/GroupProject/hoteljpamvcthymleaf/dto/CustomerDTO.java
@@ -7,15 +7,19 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+
 public class CustomerDTO implements Serializable {
     private String customerId;
     private boolean available;
+
     public boolean isAvailable() {
         return available;
     }
+
     public void setAvailable(boolean available) {
         this.available = available;
     }
+
     @NotBlank(message = "Required field")
     @Size(min = 2, max = 255, message = "Need to have at least 2 letters")
     private String firstName;
@@ -27,8 +31,10 @@ public class CustomerDTO implements Serializable {
     private String email;
     @PastOrPresent(message = "The booking date should be in the present or in the past.")
     LocalDate bookingDate;
+
     public CustomerDTO() {
     }
+
     public CustomerDTO(String customerId, @NotBlank(message = "Required field") @Size(min = 2, max = 255, message = "Need to have at least 2 letters") String firstName, @NotBlank(message = "Required field") @Size(min = 2, max = 255, message = "Need to have at least 2 letters") String lastName, @NotBlank(message = "Required field") @Email(regexp = "^(\\D)+(\\w)*((\\.(\\w)+)?)+@(\\D)+(\\w)*((\\.(\\D)+(\\w)*)+)?(\\.)[a-z]{2,}$", flags = Pattern.Flag.CASE_INSENSITIVE, message = "Email format is invalid") String email, @PastOrPresent(message = "The booking date should be in the present or in the past.") LocalDate bookingDate) {
         this.customerId = customerId;
         this.firstName = firstName;
@@ -36,15 +42,18 @@ public class CustomerDTO implements Serializable {
         this.email = email;
         this.bookingDate = bookingDate;
     }
+
     public CustomerDTO(Customer customer) {
         setBookingDate(customer.getBookingDate());
         setFirstName(customer.getCustomerFirstName());
-        setLastName(customer.getCustomerFirstName());
+        setLastName(customer.getCustomerLastName());
         setEmail(customer.getCustomerEmail());
     }
+
     public String getCustomerId() {
         return customerId;
     }
+
     public static List<CustomerDTO> toCustomerDto(List<Customer> customers) {
         List<CustomerDTO> result = new ArrayList<>();
         for (Customer customer : customers) {
@@ -53,30 +62,39 @@ public class CustomerDTO implements Serializable {
         }
         return result;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
+
     public LocalDate getBookingDate() {
         return bookingDate;
     }
+
     public void setBookingDate(LocalDate bookingDate) {
         this.bookingDate = bookingDate;
     }
+
     @Override
     public String toString() {
         return "CustomerDTO{" +
